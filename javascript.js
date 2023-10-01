deb4359372e9c09aba6e36eb59f3ebdfe1bb506a
lucianaalyah <137006718+lucianaalyah@users.noreply.github.com>
2023-10-01 16:36:18 -0300

    Desafio 6

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var n = parseInt(prompt('Digite um número:'));
+
+if(n%2 ==0 && n>0 && n< 100){
+alert('Número atende aos critérios! ♥')
+}else{
+alert('Número não atende aos critérios! :(')
+}
+alert(total);
+
